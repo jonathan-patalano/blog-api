6606c99016a9a5213d137fffbf79af5e660dcbae
jopatalano <jonathanpatalano@gmail.com>
2019-01-11 14:29:46 +0100

    feat(settings): Initialize basic files

---
+++ src/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { UserRepository } from "./user.repository";
+
+@Injectable()
+export class UserService {
+  constructor(
+    @Inject(UserRepository) private readonly userRepository: UserRepository
+  ) {}
+
+  /**
+   * Returns a user identified by its id
+   *
+   * @param id - user id
+   * @returns Resolves with User
+   */
+  async getById(id: string) {
+    return this.userRepository.findOne(id);
+  }
+}
